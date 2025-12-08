5cba0907147405a1495d5366d71b07891e86a004
Eemil <eemil.pirinen@live.fi>
2025-12-08 16:49:14 +0200

    implemented tests for clamp

---
+++ tests/other/clamp.test.js
@@ -0,0 +1,35 @@
+import clamp from '../../src/clamp.js';
+
+describe('clamp', () => {
+
+    test('clamps values to the lower bound', () => {
+        const result = clamp(-10, -5, 5);
+        expect(result).toBe(-5);
+    });
+
+    test('clamps values to the upper bound', () => {
+        const result = clamp(10, -5, 5);
+        expect(result).toBe(5);
+    });
+
+    test('returns the number if within bounds', () => {
+        const result = clamp(0, -5, 5);
+        expect(result).toBe(0);
+    });
+
+    test('resolves parameters as 0 if lower bound is NaN', () => {
+        const result = clamp(10, NaN, 5);
+        expect(result).toBe(5);
+    });
+
+    test('resolves parameters as 0 if upper bound is NaN', () => {
+        const result = clamp(-10, -5, NaN);
+        expect(result).toBe(0);
+    });
+
+    test('handles floating point numbers', () => {
+        const result = clamp(10.5, -5.5, 5.5);
+        expect(result).toBe(5.5);
+    });
+
+});
